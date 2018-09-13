4f1fa60e533f1f65ae77855f62cf87c56ebdc42d
RossPin <dorpetz@hotmail.com>
2018-09-13 12:22:21 +1200

    change quiz to survey

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,7 +10,7 @@ class Home extends React.Component {
   render(){
     return (
       <div>
-        <Link className='button' to='/question/1' >Start Quiz</Link>
+        <Link className='button' to='/question/1' >Start Survey</Link>
       </div>
     )
   }
