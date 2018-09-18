3b3e58095be41a465cbc41343e856b2d168fdd51
RossPin <dorpetz@hotmail.com>
2018-09-18 22:29:57 +1200

    login redirects to current

--- client/actions/login.js
+++ client/actions/login.js
@@ -39,7 +39,7 @@ export function loginUser (creds) {
         } else {
           const userInfo = saveUserToken(response.body.token)
           dispatch(receiveLogin(userInfo))
-          document.location = "/"
+          document.location = "/current"
         }
       }).catch(err => alert("Try Again!")
 
