46486dfab1c46ecd15149671643538edc2c3fb88
RossPin <dorpetz@hotmail.com>
2018-09-19 09:14:23 +1200

    login directs to home

--- client/actions/login.js
+++ client/actions/login.js
@@ -39,7 +39,7 @@ export function loginUser (creds) {
         } else {
           const userInfo = saveUserToken(response.body.token)
           dispatch(receiveLogin(userInfo))
-          document.location = "/current"
+          document.location = "/"
         }
       }).catch(err => alert("Try Again!")
 
