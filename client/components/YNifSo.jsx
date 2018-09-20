b76a1fafa9b0c0f7819a1846702808f54016b3e5
RossPin <dorpetz@hotmail.com>
2018-09-21 10:03:08 +1200

    create YNifSo

--- client/components/YNifSo.jsx
+++ client/components/YNifSo.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 class Radio extends React.Component {
   render () {
-    const { question, update, answer } = this.props
+    const { question, update, answer, updateIfSo } = this.props
     return (
       <div>
         <h3>{question.question}</h3>
         <form>
-          <input type='radio' name='answer' onChange={e => update(e, question.id, question.question, response.show, response.hide)}
-            value='Yes' checked={answer === 'Yes'} />Yes
-          <input type='radio' name='answer' onChange={e => update(e, question.id, question.question, response.show, response.hide)}
-            value='No' checked={answer === 'No'} />No
-          {(answer === question.ifSo.req) && <label>{question.ifSo.question}<br />
-            <input type='text' />
+          <input type='radio' name='answer' onChange={e => update(e, question.id, question.question)}
+            value='Yes' checked={answer === 'Yes'} />Yes<br />
+          <input type='radio' name='answer' onChange={e => update(e, question.id, question.question)}
+            value='No' checked={answer === 'No'} />No<br />
+          {(answer === question.ifSo.val) && <label>{question.ifSo.question}<br />
+            <input type='text' onChange={e => updateIfSo(e, question.id, question.ifSo.question)} />
           </label>}
         </form>
       </div>
