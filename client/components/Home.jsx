ca0d7551d53eba2abc1a6ec8c21d37d5fb89b0a0
imtiazy <yamin.imtiaz@vodafone.com>
2018-09-21 09:54:29 +1200

    checkbox fixes

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import CheckboxQ from './CheckboxQ';
 
 class Home extends React.Component {
   render () {
@@ -9,7 +8,6 @@ class Home extends React.Component {
         <h1>Intro</h1>
         <h3>This is a sample survedfdy</h3>
         <Link className='button' to='/consent' >Start Survey</Link>
-        <CheckboxQ />
       </div>
     )
   }
