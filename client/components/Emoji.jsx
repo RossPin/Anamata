bf2a005dfd7680355552e22abc235518b93917aa
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-09-21 11:02:40 +1200

    refactoring event handler

--- client/components/Emoji.jsx
+++ client/components/Emoji.jsx
@@ -3,21 +3,22 @@ import React from 'react'
 class Emoji extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.props.update(e, this.props.question.id, this.props.question.question)
   }
 
   render () {
     return (
       <div>
         <div>
-          <h3>{this.props.question.question}</h3>
-        </div>
-        <div>
-          <span><button className='emoji' id='sad' value='1' onClick={e => this.props.update(e, this.props.question.id, this.props.question.question)}> ☹️</button></span>
-          <span><button className='emoji' id='sad' value='2' onClick={e => this.props.update(e, this.props.question.id, this.props.question.question)}> 🙁</button></span>
-          <span><button className='emoji' id='sad' value='3' onClick={e => this.props.update(e, this.props.question.id, this.props.question.question)}> 😐</button></span>
-          <span><button className='emoji' id='sad' value='4' onClick={e => this.props.update(e, this.props.question.id, this.props.question.question)}> 🙂</button></span>
-          <span><button className='emoji' id='sad' value='5' onClick={e => this.props.update(e, this.props.question.id, this.props.question.question)}> 😁</button></span>
+          <span><button className='emoji' id='sad' value='1' onClick={this.handleChange}> ☹️</button></span>
+          <span><button className='emoji' id='semiSad' value='2' onClick={this.handleChange}> 🙁</button></span>
+          <span><button className='emoji' id='neutral' value='3' onClick={this.handleChange}> 😐</button></span>
+          <span><button className='emoji' id='semiHappy' value='4' onClick={this.handleChange}> 🙂</button></span>
+          <span><button className='emoji' id='Happy' value='5' onClick={this.handleChange}> 😁</button></span>
         </div>
       </div>
     )
