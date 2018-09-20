429fc43ea2a505ea818f5ac3944355e2902d5de2
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-09-21 10:11:29 +1200

    changing from radio to just buttons

--- client/components/Emoji.jsx
+++ client/components/Emoji.jsx
@@ -8,10 +8,47 @@ class Emoji extends React.Component {
   render () {
     return (
       <div>
-        <h4>Emoji</h4>
-
+        <button className='emoji' id='sad' value='1'> ☹️</button>
+        <button className='emoji' id='sad' value='2'> 🙁</button>
+        <button className='emoji' id='sad' value='3'> 😐</button>
+        <button className='emoji' id='sad' value='4'> 🙂</button>
+        <button className='emoji' id='sad' value='5'> 😁</button>
       </div>
     )
   }
 }
 export default Emoji
+
+
+{/* <form class='rating'>
+          <label>
+            <input type='radio' name='emoji' value='1' />
+            <span class='icon'>☹️</span>
+          </label>
+          <label>
+            <input type='radio' name='emoji' value='2' />
+            <span class='icon'>🙁</span>
+            <span class='icon'>🙁</span>
+          </label>
+          <label>
+            <input type='radio' name='emoji' value='3' />
+            <span class='icon'>😐</span>
+            <span class='icon'>😐</span>
+            <span class='icon'>😐</span>
+          </label>
+          <label>
+            <input type='radio' name='emoji' value='4' />
+            <span class='icon'>🙂</span>
+            <span class='icon'>🙂</span>
+            <span class='icon'>🙂</span>
+            <span class='icon'>🙂</span>
+          </label>
+          <label>
+            <input type='radio' name='emoji' value='5' />
+            <span class='icon'>😁</span>
+            <span class='icon'>😁</span>
+            <span class='icon'>😁</span>
+            <span class='icon'>😁</span>
+            <span class='icon'>😁</span>
+          </label>
+        </form> */}
