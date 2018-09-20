c8320b615e2ee22d576a783f3334a9d2bd1d6482
RossPin <dorpetz@hotmail.com>
2018-09-20 13:01:10 +1200

    add renderQuestion method to render based on question type

--- client/components/Questions.jsx
+++ client/components/Questions.jsx
@@ -12,6 +12,7 @@ class Question extends React.Component {
     }
     this.updateSelection = this.updateSelection.bind(this)
     this.submit = this.submit.bind(this)
+    this.renderQuestion = this.renderQuestion.bind(this)
   }
 
   updateSelection (e, id, question, show, hide) {
@@ -29,14 +30,22 @@ class Question extends React.Component {
     this.props.history.push('/complete')
   }
 
+  renderQuestion (question) {
+    switch (question.type) {
+      case 'Radio':
+        return <Radio question={question} answer={this.state.answers[question.id] ? this.state.answers[question.id].answer : null}
+          update={this.updateSelection} submit={this.submit} />
+      default:
+        return null
+    }
+  }
+
   render () {
-    const answers = this.state.answers
     return (
       <div>
         {this.state.questions.map(question => (
           <div key={question.id}>
-            {!question.conditional && <Radio question={question} answer={answers[question.id] ? answers[question.id].answer : null}
-              update={this.updateSelection} submit={this.submit} />}
+            {!question.conditional && this.renderQuestion(question)}
           </div>
         ))}
         <button className='button' onClick={this.submit} >Submit</button>
