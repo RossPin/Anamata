ee92af9e7a9ba161502dc357d7e70dc93bfe7fd7
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-09-20 16:08:13 +1200

    emoji component skel

---
+++ client/components/Emoji.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Emoji extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    return (
+      <div>
+        <h4>Emoji</h4>
+
+      </div>
+    )
+  }
+}
+export default Emoji
