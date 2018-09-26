9769de64ac4bb1a81c707ff26bcd8d30bfc6be18
RossPin <dorpetz@hotmail.com>
2018-09-26 16:59:12 +1200

    round risks to 1dp

--- client/utils/eval.js
+++ client/utils/eval.js
@@ -10,11 +10,14 @@ function getRisks (yp) {
 
 function getRisk (section) {
   const keys = Object.keys(section)
-  return keys.reduce((acc, key) => {
+  const risk = keys.reduce((acc, key) => {
     if (section[key].risk) acc.risk += section[key].risk
     if (section[key].resiliency) acc.resiliency += section[key].resiliency
     return acc
   }, { risk: 0, resiliency: 0 })
+  risk.risk = Math.round(risk.risk * 10) / 10
+  risk.resiliency = Math.round(risk.resiliency * 10) / 10
+  return risk
 }
 
 module.exports = {
