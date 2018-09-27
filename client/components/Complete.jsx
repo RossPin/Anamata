6d562cee5068412e0e339f9cd1ea45bb99f88207
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-09-27 16:51:00 +1200

    added links

--- client/components/Complete.jsx
+++ client/components/Complete.jsx
@@ -29,6 +29,13 @@ class Complete extends React.Component {
         <ul>
           <li><a href='https://www.anamatacafe.org.nz/' target='_blank'>Anamata Cafe</a></li>
           <li><a href='https://www.anamatacafe.org.nz/' target='_blank'>Anamata Cafe</a></li>
+          <li><a href='https://kidshealth.org/en/teens/love.html'target='_blank'>teen love</a></li>
+          <li><a href=' http://insideout.org.nz/'target='_blank'>insideout</a></li>
+          <li><a href='https://teentalk.ca/learn-about/consent-2/'target='_blank'>consent</a></li>
+          <li><a href='https://www.youtube.com/watch?v=pZwvrxVavnQ'target='_blank'>yt video</a></li>
+          <li><a href='https://www.safetotalk.nz/what-is-sexual-harm/?gclid=EAIaIQobChMIrKuejsDI3QIVBqqWCh3PwgQREAAYASAAEgJ2efD_BwE'target='_blank'>safe to talk</a></li>
+          <li><a href='https://www.youthline.co.nz/'target='_blank'>youthline Free call 0800 376 633 | Free text 234</a></li>
+          <li><a href='https://kidshealth.org/en/teens/healthy-relationship.html'target='_blank'>kid health</a></li>
         </ul>
         <button className='button' onClick={this.submit}>Finish</button>
       </div>
