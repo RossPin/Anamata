7c0433a22dcfeedd785cc61df9fb64804f1b87ca
imtiazy <yamin.imtiaz@vodafone.com>
2018-09-27 15:08:14 +1200

    feedback added

--- client/components/Help.jsx
+++ client/components/Help.jsx
@@ -3,6 +3,22 @@ import { connect } from 'react-redux'
 import { setStyle } from '../actions/style'
 
 class Help extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      feedback: ''
+    }
+    this.updateDetails = this.updateDetails.bind(this)
+  }
+
+  updateDetails (e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render () {
     return (
       <div className='container help'>
@@ -21,6 +37,15 @@ class Help extends React.Component {
           molestie aliquet vel in erat. Nulla dignissim at ex pretium finibus.
           Phasellus eu enim tempus, tristique justo fermentum, elementum nulla.
         </p>
+        <div className='helpfeedback'>
+          <h1>Feedback</h1>
+          <form onSubmit={this.submit}>
+            <TextDetails detail='Name' name='name' onChange={this.updateDetails} />
+            <TextDetails detail='Email' name='email' onChange={this.updateDetails} />
+            <TextDetails detail='Feedback' name='feedback' onChange={this.updateDetails} className='feedback' />
+            <input className='button' type='submit' value='Submit' />
+          </form>
+        </div>
       </div>
     )
   }
@@ -28,5 +53,11 @@ class Help extends React.Component {
     this.props.dispatch(setStyle('help_background'))
   }
 }
+
+const TextDetails = ({ detail, name, onChange, className }) =>
+  <div className='textDetails'>
+    <label className='label' htmlFor={name}>{detail}</label>
+    <input className={className} id={name} type='text' name={name} onChange={onChange} />
+  </div>
 
 export default connect()(Help)
