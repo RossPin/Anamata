32b71d900a5760800bd5721e2520a09e08b6b4bd
mattjackmcgregor <mattjackmcgregor@gmail.com>
2018-09-27 19:32:03 +1200

    custom svg checkbox but only one working cause of id

--- client/components/Checkbox.jsx
+++ client/components/Checkbox.jsx
@@ -26,19 +26,18 @@ class Checkbox extends React.Component {
   render () {
     const { question, update, answer } = this.props
     return (
-      <div>
-        {question.tooltip ? this.tooltip(question) : <h3>{question.question}</h3>}
-        <form>
+      <div className='qDiv' data-aos='fade-right'>
+        <div className='speech-bubble'>
+          {question.tooltip ? this.tooltip(question) : <h3>{question.question}</h3>}
+        </div>
+        <form data-aos='fade-left'>
           {question.responses.map((response, i) => (
-            <div key={i}>
-              <label className='checkbox'>
-                <input type='checkbox' id='answer' name='answer'
-                  value={response.answer} checked={answer[response.answer]
-                    ? answer[response.answer] : false} onChange={e => update(e, question)} />
-                <svg width='20' height='20' viewBox='0 0 20 20'>
-                  <rect width='20' height='20' />
-                  <path d='M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z' class='inner' />
-                </svg>
+            <div className='checkbox'key={i}>
+              <input type='checkbox' id='checkbox' name='answer'
+                value={response.answer} checked={answer[response.answer]
+                  ? answer[response.answer] : false} onChange={e => update(e, question)} />
+              <label for='checkbox'>
+                <svg viewBox='0 0 60 40'><path d='M21,2 C13.4580219,4.16027394 1.62349378,18.3117469 3,19 C9.03653312,22.0182666 25.2482171,10.3758914 30,8 C32.9363621,6.53181896 41.321398,1.67860195 39,4 C36.1186011,6.8813989 3.11316157,27.1131616 5,29 C10.3223659,34.3223659 30.6434647,19.7426141 35,18 C41.2281047,15.5087581 46.3445303,13.6554697 46,14 C42.8258073,17.1741927 36.9154967,19.650702 33,22 C30.3136243,23.6118254 17,31.162498 17,34 C17,40.4724865 54,12.4064021 54,17 C54,23.7416728 34,27.2286213 34,37' stroke-width='4' fill='none' stroke-dasharray='270' stroke-dashoffset='270' /></svg>
                 {response.answer}
               </label>
             </div>
